47d238810d821aa34265743a0f6581d012f06cdd
pinar <evinpinarornek@gmail.com>
2016-11-16 01:25:06 +0200

    init project

--- TagLifeBackend/TagLifeApp/views.py
+++ TagLifeBackend/TagLifeApp/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from TagLifeApp.models import Entry, Topic
+import TagLifeApp.models
 
 
 def index(request):
     context = RequestContext(request)
 
-    topic_list = Topic.objects.order_by('title')
+    topic_list = TagLifeApp.models.Topic.objects.order_by('title')
     context_dict = {'topics': topic_list}
 
     for topic in topic_list:
@@ -28,13 +28,13 @@ def topic(request, topic_name_url):
     context_dict = {"topic_name": topic_name}
 
     try:
-        topic = Topic.objects.get(title=topic_name)
-        entrys = Entry.objects.filter(topic=topic)
+        topic = TagLifeApp.models.Topic.objects.get(title=topic_name)
+        entrys = TagLifeApp.models.Entry.objects.filter(topic=topic)
 
         context_dict['entrys'] = entrys
         context_dict['topic'] = topic_name
 
-    except Topic.DoesNotExist:
+    except TagLifeApp.models.Topic.DoesNotExist:
         pass
 
     return render_to_response('topic.html', context_dict, context)
