5d6ea90a5de3dba3c7b1509f041b6a2f8174f49c
Batuhan Demir <dmrbatu23@gmail.com>
2016-12-11 15:16:00 +0300

    register added

--- TaglifeRest/rest-frontend/static/js/signup.js
+++ TaglifeRest/rest-frontend/static/js/signup.js
@@ -64,3 +64,83 @@ $(function () {
         init();
     });
 });
+/*
+{
+    "username": "",
+    "first_name": "",
+    "last_name": "",
+    "email": "",
+    "password": ""
+}
+ */
+$('#register_form').submit(function(event) {
+
+  var userObj = {};
+  var username = $(this).find("input[name='user_name']").val() ;
+  var firstname = $(this).find("input[name='first_name']").val() ;
+  var lastname = $(this).find("input[name='last_name']").val() ;
+  var email = $(this).find("input[name='email']").val() ;
+  var password = $(this).find("input[name='password']").val() ;
+  userObj['username'] = username ;
+  userObj['first_name'] = firstname ;
+  userObj['last_name'] = lastname ;
+  userObj['email'] = email ;
+  userObj['password'] = password ;
+
+  var userJSON = JSON.stringify(userObj);
+
+  $.ajax({
+    type: "POST",
+    url: "http://custom-env.dpwai7zqmg.us-west-2.elasticbeanstalk.com/users/create/",
+    data: userJSON,
+    dataType: "json",
+    contentType: "application/json",
+    error: function(xhr, textStatus, error) {
+        console.log(xhr.statusText);
+          console.log(textStatus);
+          console.log(error);
+    },
+    success: function(data) {
+      alert("USER SUCCESFULLY ADDED");
+      console.log(data);
+    }
+  });
+
+  return false;
+});
+
+$('#login_form').submit(function(event) {
+
+  var userObj = {};
+  var username = $(this).find("input[name='user_name']").val() ;
+  var firstname = $(this).find("input[name='first_name']").val() ;
+  var lastname = $(this).find("input[name='last_name']").val() ;
+  var email = $(this).find("input[name='email']").val() ;
+  var password = $(this).find("input[name='password']").val() ;
+  userObj['username'] = username ;
+  userObj['first_name'] = firstname ;
+  userObj['last_name'] = lastname ;
+  userObj['email'] = email ;
+  userObj['password'] = password ;
+
+  var userJSON = JSON.stringify(userObj);
+
+  $.ajax({
+    type: "POST",
+    url: "http://custom-env.dpwai7zqmg.us-west-2.elasticbeanstalk.com/users/create/",
+    data: userJSON,
+    dataType: "json",
+    contentType: "application/json",
+    error: function(xhr, textStatus, error) {
+        console.log(xhr.statusText);
+          console.log(textStatus);
+          console.log(error);
+    },
+    success: function(data) {
+      alert("USER SUCCESFULLY ADDED");
+      console.log(data);
+    }
+  });
+
+  return false;
+});
