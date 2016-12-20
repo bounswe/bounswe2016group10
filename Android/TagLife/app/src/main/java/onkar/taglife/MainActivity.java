47ffa5b3ea9eb04f1806f04afafb554467312dc7
osmannnurikar <nuri.kar@boun.edu.tr>
2016-12-20 21:50:21 +0300

    androıd project

--- Android/TagLife/app/src/main/java/onkar/taglife/MainActivity.java
+++ Android/TagLife/app/src/main/java/onkar/taglife/MainActivity.java
@@ -221,6 +221,7 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        int id2 =5;
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_profile) {
@@ -234,6 +235,7 @@ public class MainActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
+
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
