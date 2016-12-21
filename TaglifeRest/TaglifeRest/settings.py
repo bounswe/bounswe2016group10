7e0eb56a9629361d178654a5ad29ea366ddd99ee
Tuna Han Salih Meral <tunahansmeral@gmail.com>
2016-12-21 20:42:51 +0300

    pagination set to 1000

--- TaglifeRest/TaglifeRest/settings.py
+++ TaglifeRest/TaglifeRest/settings.py
@@ -152,5 +152,5 @@ REST_FRAMEWORK = {
          #'rest_framework.authentication.TokenAuthentication',
         ),
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
-    'PAGE_SIZE': 0
+    'PAGE_SIZE': 1000
 }
